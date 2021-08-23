7b8eaedb1e8cbe84d22d3cea3c50941a71617f58
Chris <chrisbreuer93@gmail.com>
2021-08-23 11:55:04 -0700

    chore: add new homebrew path

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$DOTFILES/bin:$PATH"
 # Load Composer tools
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
+# Load Homebrew tools
+export PATH="/opt/homebrew/bin:$PATH"
+
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
