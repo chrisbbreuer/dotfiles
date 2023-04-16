559b88b54542d148c19621a6a8b485434c2ceab8
Chris Breuer <chrisbreuer93@gmail.com>
2023-04-15 22:10:52 -0700

    chore: remove node path

--- path.zsh
+++ path.zsh
@@ -7,9 +7,6 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load Homebrew tools
 export PATH="/opt/homebrew/bin:$PATH"
 
-# Load Node global installed binaries
-export PATH="$HOME/.node/bin:$PATH"
-
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
