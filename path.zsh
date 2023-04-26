101d16e8a22379d6a6ef3cf5b5e35ddf5f6421f0
Chris <chrisbreuer93@gmail.com>
2023-04-26 15:51:59 -0700

    feat: use local scripts before global ones

--- path.zsh
+++ path.zsh
@@ -10,7 +10,5 @@ export PATH="/opt/homebrew/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
-# Make sure coreutils are loaded before system commands
-# I've disabled this for now because I only use "ls" which is
-# referenced in my aliases.zsh file directly.
-#export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# Use local scripts before global ones (e.g. when buddy is installed via brew)
+export PATH=".:$PATH"
