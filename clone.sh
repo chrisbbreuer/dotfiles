cc4c335f025357981c243082a9fac7cadf6a1601
Chris <chrisbreuer93@gmail.com>
2024-03-19 13:08:18 -0700

    chore: housekeeping

--- clone.sh
+++ clone.sh
@@ -6,10 +6,7 @@ SITES=$HOME/Code
 
 # Open Web
 git clone git@github.com:stacksjs/stacks.git $SITES/stacks
-git clone git@github.com:ow3org/vue-starter.git $SITES/vue-starter
-git clone git@github.com:ow3org/ts-starter.git $SITES/ts-starter
-git clone git@github.com:ow3org/web-components-starter.git $SITES/web-components-starter
-git clone git@github.com:ow3org/composable-starter.git $SITES/composable-starter
-
-# CION
-git clone git@github.com:ci-on/cion.agency.git $SITES/cion.agency
+git clone git@github.com:stacksjs/ts-starter.git $SITES/ts-starter
+git clone git@github.com:stacksjs/dynamodb-tooling.git $SITES/dynamodb-tooling
+git clone git@github.com:stacksjs/tlsx.git $SITES/tlsx
+git clone git@github.com:stacksjs/reverse-proxy.git $SITES/reverse-proxy
